093b5a4ac22758a812d6452185c0eb905e616bce
Aiden Cullo <aidencullo@gmail.com>
2024-09-17 10:37:15 -0400

    utilize jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import PerScholasLogo from "./PerScholasLogo.js";
 import TableOfContents from "./TableOfContents.js";
 
+const element = <h1>Hello, world!</h1>;
+
 export default function App() {
   return <>
 	   <PerScholasLogo />
 	   <TableOfContents />
+	   { element }
 	 </>;
 }
